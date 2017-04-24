c56982c177cc02c58faf706b99ce05b74d2371e1
chaxiaomao <1145470390@qq.com>
2017-04-24 10:56:16 +0800

    fix_20170424

--- resources/views/shop/home.blade.php
+++ resources/views/shop/home.blade.php
@@ -3,16 +3,50 @@
 @section('title','首页')
 
 @section('content')
+    <ul class="carousel" ontouchstart="">
+        <li class="item">
+            <img src="/imgs/b1.png">
+        </li>
+        <li class="item">
+            <img src="/imgs/b2.png">
+        </li>
+        <li class="item">
+            <img src="/imgs/b3.png">
+        </li>
+        <li class="item">
+            <img src="/imgs/b4.png">
+        </li>
+    </ul>
+    <ul class="page-number">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    <div class="shell">
+        @foreach($products as $product)
+            <a><img src="{{$product->display}}"/></a>
+        @endforeach
+    </div>
 
-    @foreach($products as $product)
-        <img src="{{$product->display}}" />
-    @endforeach
 @endsection
 
 @section('m-js')
-<script>
-    $("#personal").attr("href","{{url('personal')}}");
-    $("#cart").attr("href","{{url('cart')}}");
-
-</script>
+    <script src="/js/LinkList.js"></script>
+    <script src="/js/carousel.js"></script>
+    <script>
+        $("#personal").attr("href", "{{url('personal')}}");
+        $("#cart").attr("href", "{{url('cart')}}");
+    </script>
+    <script>
+        var carousel = CreateCarousel("carousel", "item", true).bindTouchEvent().setItemChangedHandler(onPageChanged);
+        var dots = document.querySelectorAll(".page-number li");
+        var curDot = dots[0];
+        curDot.className = "active";
+        function onPageChanged(preIndex, curIndex) {
+            curDot.className = "";
+            curDot = dots[curIndex];
+            curDot.className = "active";
+        }
+    </script>
 @endsection
